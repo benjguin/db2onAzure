e68eca428586057b8fd5d4607c35b1ba1c8b1327
Alessandro Vozza <alessandro.vozza@microsoft.com>
2018-04-05 11:38:44 +0200

    more scripts

--- install_config_gluster.sh
+++ install_config_gluster.sh
@@ -2,8 +2,7 @@
 
 #install and configure gluster
 
-yum update -y
-
+#setup DNS static
 cat >> /etc/hosts <<EOF
 192.168.1.10 g1
 192.168.1.11 g1
@@ -14,8 +13,20 @@ cat >> /etc/hosts <<EOF
 192.168.2.12 g3b
 EOF
 
-yum install glusterfs glusterfs-cli glusterfs-libs glusterfs -y
+#Install stuff
+yum update -y
+cat >> /etc/yum.repos.d/Gluster.repo <<EOF
+[gluster38]
+name=Gluster 3.8
+baseurl=http://mirror.centos.org/centos/7/storage/$basearch/gluster-3.8/
+gpgcheck=0
+enabled=1
+EOF
 
+yum install glusterfs glusterfs-cli glusterfs-libs  -y
+yum install -y  glusterfs-cli glusterfs-libs glusterfs-server
+
+#prepare the disks
 pvcreate /dev/sdc
 pvcreate /dev/sdd
 vgcreate vg_gluster /dev/sdc /dev/sdd
@@ -31,3 +42,17 @@ mkdir -p /bricks/db2quorum
 
 mount /dev/vg_gluster/brick1 /bricks/db2data/
 mount /dev/vg_gluster/brick2 /bricks/db2quorum/
+
+cat >> /etc/fstab <<EOF
+/dev/vg_gluster/brick1  /bricks/db2data    xfs     defaults    0 0
+/dev/vg_gluster/brick2  /bricks/db2quorum    xfs     defaults    0 0
+EOF
+
+#firewall
+firewall-cmd --zone=public --add-port=24007-24008/tcp --permanent
+firewall-cmd --zone=public --add-port=49152-49160/tcp --permanent
+firewall-cmd --reload
+
+#start gluster
+systemctl enable glusterd
+systemctl start glusterd
