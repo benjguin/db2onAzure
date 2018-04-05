caa65e8af3b2733a1552590e8610c592678a77bc
Alessandro Vozza <alessandro.vozza@microsoft.com>
2018-04-05 10:04:18 +0200

    removed eth0 section

--- start_network.sh
+++ start_network.sh
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #!/bin/bash
 
 #adapted from https://docs.microsoft.com/en-us/azure/virtual-machines/linux/multiple-nics
@@ -9,15 +8,6 @@ lastip=`echo ${ip//*.}`
 echo "200 eth0-rt" >> /etc/iproute2/rt_tables
 echo "201 eth1-rt" >> /etc/iproute2/rt_tables
 
-cat >  /etc/sysconfig/network-scripts/ifcfg-eth0 << EOF
-BOOTPROTO=dhcp  
-DEVICE=eth0
-ONBOOT=yes
-TYPE=Ethernet
-USERCTL=no
-NM_CONTROLLED=no  
-EOF
-
 cat >  /etc/sysconfig/network-scripts/ifcfg-eth1 << EOF
 BOOTPROTO=dhcp  
 DEVICE=eth1
@@ -50,45 +40,3 @@ EOF
 systemctl restart network
 
 dhclient
-=======
-#!/bin/bash
-
-ip=`hostname -I`
-lastip=`echo ${ip//*.}`
-
-echo "200 eth0-rt" >> /etc/iproute2/rt_tables
-echo "201 eth1-rt" >> /etc/iproute2/rt_tables
-
-cat >  /etc/sysconfig/network-scripts/ifcfg-eth1 << EOF
-BOOTPROTO=dhcp  
-DEVICE=eth1
-ONBOOT=yes
-TYPE=Ethernet
-USERCTL=no
-NM_CONTROLLED=no  
-EOF
-
-cat > /etc/sysconfig/network-scripts/rule-eth0 << EOF
-from 192.168.1.$lastip/32 table eth0-rt
-to 192.168.1.$lastip/32 table eth0-rt
-EOF
-
-cat > /etc/sysconfig/network-scripts/route-eth0 << EOF
-192.168.1.0/24 dev eth0 table eth0-rt
-default via 0.0.0.0 dev eth0 table eth0-rt
-EOF
-
-cat > /etc/sysconfig/network-scripts/rule-eth1 << EOF
-from 192.168.3.$lastip/32 table eth1-rt
-to 192.168.3.$lastip/32 table eth1-rt
-EOF
-
-cat > /etc/sysconfig/network-scripts/route-eth1 << EOF
-192.168.2.0/24 dev eth1 table eth1-rt
-default via 0.0.0.0 dev eth1 table eth1-rt
-EOF
-
-systemctl restart network
-
-dhclient
->>>>>>> be9db2039d413eb39c7ec8fd2d78ee7a624e4bd2
