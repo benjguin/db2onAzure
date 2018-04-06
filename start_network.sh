6aac4e75f37dc67711885d3ae6d0771d28b63ede
benjguin <benjamin@3-4.fr>
2018-04-06 10:47:27 +0200

    add eth2 support

--- start_network.sh
+++ start_network.sh
@@ -2,8 +2,17 @@
 
 #adapted from https://docs.microsoft.com/en-us/azure/virtual-machines/linux/multiple-nics
 
-ip=`hostname -I`
-lastip=`echo ${ip//*.}`
+dhclient
+
+eth0ip=`ifconfig eth0 | awk '$1 == "inet" {print $2}'`
+IFS="." read -ra eth0ipparts <<< "$eth0ip"
+
+eth1ip=`ifconfig eth1 | awk '$1 == "inet" {print $2}'`
+IFS="." read -ra eth1ipparts <<< "$eth1ip"
+
+lastip=`echo ${eth0ipparts[3]}`
+eth0subnetip=`echo ${eth0ipparts[2]}`
+eth1subnetip=`echo ${eth1ipparts[2]}`
 
 echo "200 eth0-rt" >> /etc/iproute2/rt_tables
 echo "201 eth1-rt" >> /etc/iproute2/rt_tables
@@ -18,25 +27,23 @@ NM_CONTROLLED=no
 EOF
 
 cat > /etc/sysconfig/network-scripts/rule-eth0 << EOF
-from 192.168.1.$lastip/32 table eth0-rt
-to 192.168.1.$lastip/32 table eth0-rt
+from 192.168.$eth0subnetip.$lastip/32 table eth0-rt
+to 192.168.$eth0subnetip.$lastip/32 table eth0-rt
 EOF
 
 cat > /etc/sysconfig/network-scripts/route-eth0 << EOF
-192.168.1.0/24 dev eth0 table eth0-rt
+192.168.$eth0subnetip.0/24 dev eth0 table eth0-rt
 default via 0.0.0.0 dev eth0 table eth0-rt
 EOF
 
 cat > /etc/sysconfig/network-scripts/rule-eth1 << EOF
-from 192.168.3.$lastip/32 table eth1-rt
-to 192.168.3.$lastip/32 table eth1-rt
+from 192.168.$eth1subnetip.$lastip/32 table eth1-rt
+to 192.168.$eth1subnetip.$lastip/32 table eth1-rt
 EOF
 
 cat > /etc/sysconfig/network-scripts/route-eth1 << EOF
-192.168.3.0/24 dev eth1 table eth1-rt
+192.168.$eth1subnetip.0/24 dev eth1 table eth1-rt
 default via 0.0.0.0 dev eth1 table eth1-rt
 EOF
 
 systemctl restart network
-
-dhclient
