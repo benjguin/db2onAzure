d3d01e43d6f8d827430bbc8e37b2478f825e2027
benjguin <benjamin@3-4.fr>
2018-05-18 16:56:23 +0200

    wip

--- deployment/initscripts/db2.sh
+++ deployment/initscripts/db2.sh
@@ -6,10 +6,10 @@ rhelPubKeyValue=$3
 rootPrivKeyValue=$4
 rootPubKeyValue=$5
 db2bits=$6
+logPath=$7
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-sudo -n -u root bash -c "bash -v ${DIR}/startnetwork_root.sh"
 
-bash -v ${DIR}/setsshkeys.sh "$userPubKeyValue" "$rhelPrivKeyValue" "$rhelPubKeyValue" "$rootPrivKeyValue" "$rootPubKeyValue"
-
-sudo -n -u root bash -c "bash -v ${DIR}/db2_root.sh \"$db2bits\""
+sudo -n -u root bash -c "bash -v ${DIR}/startnetwork_root.sh" &> >(tee -a $logPath)
+bash -v ${DIR}/setsshkeys.sh "$userPubKeyValue" "$rhelPrivKeyValue" "$rhelPubKeyValue" "$rootPrivKeyValue" "$rootPubKeyValue" &> >(tee -a $logPath)
+sudo -n -u root bash -c "bash -v ${DIR}/db2_root.sh \"$db2bits\"" &> >(tee -a $logPath)
--- deployment/initscripts/glusterfs.sh
+++ deployment/initscripts/glusterfs.sh
@@ -5,10 +5,10 @@ rhelPrivKeyValue=$2
 rhelPubKeyValue=$3
 rootPrivKeyValue=$4
 rootPubKeyValue=$5
+logPath=$6
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-sudo -n -u root bash -c "bash -v ${DIR}/startnetwork_root.sh"
 
-bash -v ${DIR}/setsshkeys.sh "$userPubKeyValue" "$rhelPrivKeyValue" "$rhelPubKeyValue" "$rootPrivKeyValue" "$rootPubKeyValue"
-
-sudo -n -u root bash -c "bash -v ${DIR}/installconfiggluster_root.sh"
+sudo -n -u root bash -c "bash -v ${DIR}/startnetwork_root.sh" &> >(tee -a $logPath)
+bash -v ${DIR}/setsshkeys.sh "$userPubKeyValue" "$rhelPrivKeyValue" "$rhelPubKeyValue" "$rootPrivKeyValue" "$rootPubKeyValue" &> >(tee -a $logPath)
+sudo -n -u root bash -c "bash -v ${DIR}/installconfiggluster_root.sh" &> >(tee -a $logPath)
