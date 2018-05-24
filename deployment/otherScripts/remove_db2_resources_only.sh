c23167b20d44e192612c2c5aa78f8b7d828612e1
benjguin <benjamin@3-4.fr>
2018-05-24 09:48:09 +0200

    setup works with ARM

--- deployment/otherScripts/remove_db2_resources_only.sh
+++ deployment/otherScripts/remove_db2_resources_only.sh
@@ -2,36 +2,31 @@
 
 rg=$1
 
-az vm delete -g $rg --name d0
-az vm delete -g $rg --name d1
-az vm delete -g $rg --name cf0
-az vm delete -g $rg --name cf1
+az vm delete -y -g $rg --name d0
+az vm delete -y -g $rg --name d1
+az vm delete -y -g $rg --name cf0
+az vm delete -y -g $rg --name cf1
 
-az network nic delete -g $rg --name cf0_db2be
-az network nic delete -g $rg --name cf0_gfsfe
-az network nic delete -g $rg --name cf0_main
-az network nic delete -g $rg --name cf1_db2be 
-az network nic delete -g $rg --name cf1_gfsfe    
-az network nic delete -g $rg --name cf1_main     
-az network nic delete -g $rg --name d0_db2be     
-az network nic delete -g $rg --name d0_db2fe     
-az network nic delete -g $rg --name d0_gfsfe     
-az network nic delete -g $rg --name d0_main      
-az network nic delete -g $rg --name d1_db2be     
-az network nic delete -g $rg --name d1_db2fe     
-az network nic delete -g $rg --name d1_gfsfe     
-az network nic delete -g $rg --name d1_main      
+az network nic delete -g $rg --name cf0_db2be &
+az network nic delete -g $rg --name cf0_gfsfe &
+az network nic delete -g $rg --name cf0_main &
+az network nic delete -g $rg --name cf1_db2be &
+az network nic delete -g $rg --name cf1_gfsfe &  
+az network nic delete -g $rg --name cf1_main &  
+az network nic delete -g $rg --name d0_db2be &  
+az network nic delete -g $rg --name d0_db2fe &  
+az network nic delete -g $rg --name d0_gfsfe &  
+az network nic delete -g $rg --name d0_main &  
+az network nic delete -g $rg --name d1_db2be &  
+az network nic delete -g $rg --name d1_db2fe &  
+az network nic delete -g $rg --name d1_gfsfe &  
+az network nic delete -g $rg --name d1_main &  
 
-az disk delete -g $rg --name cf0_dataDisk_lun0
-az disk delete -g $rg --name cf0_osDisk       
-az disk delete -g $rg --name cf1_dataDisk_lun0
-az disk delete -g $rg --name cf1_osDisk       
-az disk delete -g $rg --name d0_dataDisk_lun0 
-az disk delete -g $rg --name d0_osDisk        
-az disk delete -g $rg --name d1_dataDisk_lun0 
-az disk delete -g $rg --name d1_osDisk        
-
-az vm extension delete -g $rg --vm-name d0 --name initDb2MemberVm0
-az vm extension delete -g $rg --vm-name d0 --name initDb2MemberVm1
-az vm extension delete -g $rg --vm-name cf0 --name initDb2CfVm0
-az vm extension delete -g $rg --vm-name cf1 --name initDb2CfVm1
+az disk delete --yes -g $rg --name cf0_dataDisk_lun0 &
+az disk delete --yes -g $rg --name cf0_osDisk &
+az disk delete --yes -g $rg --name cf1_dataDisk_lun0 &
+az disk delete --yes -g $rg --name cf1_osDisk &
+az disk delete --yes -g $rg --name d0_dataDisk_lun0 &
+az disk delete --yes -g $rg --name d0_osDisk &
+az disk delete --yes -g $rg --name d1_dataDisk_lun0 &
+az disk delete --yes -g $rg --name d1_osDisk &
