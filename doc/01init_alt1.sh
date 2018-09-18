77241d9c96e6995af3a85f892065bb0b16761e48
Stéphane Erbrech <stephane.erbrech@gmail.com>
2018-09-18 10:57:18 +0200

    add comment to 01init_alt1

--- doc/01init_alt1.sh
+++ doc/01init_alt1.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-#This contains an alternative example to 01init.sh
+
+# in the init-private, I set $lisbits and $db2bits urls to reuse existing ones
 . ./01init-private.sh
+
+# runs the default init
 . ./01init.sh
+
+# overrides some values to match an alternate environment 
 subscription="Microsoft Azure Internal Consumption"
 localGitFolderpath=/mnt/c/proj/
 tempLocalFolder=/mnt/c/tmp/db2
